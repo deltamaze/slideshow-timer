c30667d47f5ff1ea3e8a06065e3acc39b865a5f2
deltamaze <wcpooley@gmail.com>
2020-05-18 09:50:37 -0500

    threw a few more things into appstate

--- blazor/State/AppState.cs
+++ blazor/State/AppState.cs
@@ -8,6 +8,9 @@ namespace blazor.State
     public class AppState
     {
         public int SelectedNumber { get; private set; }
+        public int TimeAmount { get; private set; }
+        public int TimeLeft { get; private set; }
+        public int StopWatchTime { get; private set; }
 
         public event Action OnChange;
 
@@ -16,7 +19,22 @@ namespace blazor.State
             SelectedNumber = newNumber;
             NotifyStateChanged();
         }
+        public void StartTimer()
+        {
 
+        }
+        public void StopTimer()
+        {
+
+        }
+        public void ResetTimer()
+        {
+
+        }
+        public void SetTimer()
+        {
+
+        }
         private void NotifyStateChanged() => OnChange?.Invoke();
     }
 }
